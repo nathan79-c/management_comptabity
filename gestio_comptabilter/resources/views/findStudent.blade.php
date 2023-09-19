259021f203fed37893e746534dd1417f0439115e
nathan79-c <19kt219@esisalama.org>
2023-09-19 10:34:57 +0200

    importer de ficher

--- gestio_comptabilter/resources/views/findStudent.blade.php
+++ gestio_comptabilter/resources/views/findStudent.blade.php
@@ -5,5 +5,28 @@
         </h2>
     </x-slot>
 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <form class="w-full">
+                    <div class="flex justify-between items-center">
+                        <input type="text" class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" placeholder="Rechercher...">
+                        <select class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+                            <option value="all">Toutes les catégories</option>
+                            <option value="licence1">Licence 1</option>
+                            <option value="licence2">Licence 2</option>
+                            <option value="licence3">Licence 3</option>
+                            <option value="licence4">Licence 4</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="ml-auto px-4 py-2 bg-blue-500 text-black text-xl rounded-md border-radius-5">Rechercher</button>
+                </form>
 
+
+
+
+            </div>
+        </div>
+    </div>
 </x-app-layout>
+
